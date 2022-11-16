82be197f4d0ccc7dc30500d017438e1a7c803711
allankenny <allankennedy.fernandes@gmail.com>
2022-11-16 12:05:57 -0300

    Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
           />
           <Route 
             path='*' 
-            element={<NotFound />} 
+            element={<Login />} 
           />
         </Routes>
       </BrowserRouter>
